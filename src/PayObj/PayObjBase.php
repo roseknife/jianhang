07fb185dbd4f9f6e897560a9ed23fb51b6d39d93
roseknife <2786103@qq.com>
2018-06-03 19:03:11 +0800

    修改商户号

---
+++ src/PayObj/PayObjBase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RoseKnife Hua
+ * Date: 2018/6/3 0003
+ * Time: 18:58
+ */
+
+namespace RoseKnife\Jianhang\PayObj;
+
+
+class PayObjBase
+{
+    /**
+     * @var 商户编号
+     */
+    public $CUST_NO;
+}
